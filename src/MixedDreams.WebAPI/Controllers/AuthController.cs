4c0446709250a275241f329b649520f39629fd93
Eugene Cherepko <evgeniiycherepko@gmail.com>
2023-05-16 12:02:04 +0300

    validation added to auth

--- src/MixedDreams.WebAPI/Controllers/AuthController.cs
+++ src/MixedDreams.WebAPI/Controllers/AuthController.cs
@@ -10,6 +10,10 @@ using MixedDreams.Application.Features.AuthFeatures.RegisterCustomer;
 using MixedDreams.Application.Features.AuthFeatures.RegisterCompany;
 using MixedDreams.Application.Features.AuthFeatures.Login;
 using MixedDreams.Application.Features.AuthFeatures;
+using MixedDreams.Application.Common;
+using MixedDreams.Application.Features.IngredientFeatures.PostIngredient;
+using FluentValidation.Results;
+using FluentValidation;
 
 namespace MixedDreams.WebAPI.Controllers
 {
@@ -18,16 +22,27 @@ namespace MixedDreams.WebAPI.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthService _authService;
+        private readonly IValidator<CustomerRegisterRequest> _registerCustomerValidator;
+        private readonly IValidator<CompanyRegisterRequest> _registerCompanyValidator;
+        private readonly IValidator<LoginRequest> _loginValidator;
 
-        public AuthController(IAuthService authService)
+        public AuthController(IAuthService authService, IValidator<CustomerRegisterRequest> registerCustomerValidator, IValidator<CompanyRegisterRequest> registerCompanyValidator, IValidator<LoginRequest> loginValidator)
         {
             _authService = authService;
+            _registerCustomerValidator = registerCustomerValidator;
+            _registerCompanyValidator = registerCompanyValidator;
+            _loginValidator = loginValidator;
         }
 
         [HttpPost]
         [Route("register/customer")]
         public async Task<IActionResult> RegisterCustomer(CustomerRegisterRequest model)
         {
+            ValidationResult validationResult = await _registerCustomerValidator.ValidateAsync(model);
+            if (!validationResult.IsValid)
+            {
+                ErrorsMaker.ProcessValidationErrors(validationResult.Errors);
+            }
             TokenResponse response = await _authService.RegisterCustomerAsync(model);
 
             return Ok(response);
@@ -37,6 +52,11 @@ namespace MixedDreams.WebAPI.Controllers
         [Route("register/company")]
         public async Task<IActionResult> RegisterCompany([FromBody] CompanyRegisterRequest model)
         {
+            ValidationResult validationResult = await _registerCompanyValidator.ValidateAsync(model);
+            if (!validationResult.IsValid)
+            {
+                ErrorsMaker.ProcessValidationErrors(validationResult.Errors);
+            }
             TokenResponse response = await _authService.RegisterCompanyAsync(model);
 
             return Ok(response);
@@ -45,6 +65,11 @@ namespace MixedDreams.WebAPI.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(LoginRequest model)
         {
+            ValidationResult validationResult = await _loginValidator.ValidateAsync(model);
+            if (!validationResult.IsValid)
+            {
+                ErrorsMaker.ProcessValidationErrors(validationResult.Errors);
+            }
             TokenResponse response = await _authService.LoginUserAsync(model);
 
             return Ok(response);
